f5274983282c2a93a700ebdbaddf4e65c19645dc
Gadisa Amenu <amegadbay@gmail.com>
2021-11-24 03:43:13 +0300

    imporving models of customers

--- customers/models.py
+++ customers/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from rest_framework import  viewsets
+import uuid
 
 # Create your models here.
 class Customers(models.Model):
-    id = models.UUIDField(primary_key= True, editable=False ,null=False,blank=False)
+    id = models.UUIDField(primary_key= True, editable=False ,default=uuid.uuid4)
     name = models.CharField(max_length=30)
     age = models.IntegerField(max_length=3)
     orders = models.CharField()
+
+
+    def __str__(self):
+        return self.name
 
